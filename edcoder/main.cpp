a1272e06cf2b9be4a7367d46a55a87230ae6f70b
Denis <zmreact@gmail.com>
2017-04-14 14:23:09 +0300

    Конкретизировал проект для работы с изображениями.

--- edcoder/main.cpp
+++ edcoder/main.cpp
@@ -16,17 +16,17 @@ int main(int argc, char *argv[])
 {
     QTextStream out(stdout);
     QCoreApplication ed(argc, argv);
-    ed.setApplicationName("Encoder/Decoder");
+    ed.setApplicationName("Image Encoder/Decoder");
     ed.setApplicationVersion("1.0");
 
 
     /* parser declaration */
     QCommandLineParser parser;
     QCommandLineOption encode(QStringList() << "e" << "encode",
-            "Encode string <string>",
+            "Encode <image file to encode>",
             "string");
     QCommandLineOption decode(QStringList() << "d" << "decode",
-            "Decode binary code <code>",
+            "Decode <image file to decode>",
             "code");
     QCommandLineOption help(QStringList() << "h" << "help",
             "Shows this help");
@@ -47,8 +47,14 @@ int main(int argc, char *argv[])
 
     /* welcome message */
     QStringList WelcomeMessageQSL;
-    WelcomeMessageQSL << "Moscow Technical University of Communication and Informatics" << "\n" << "Department of multimedia networks and communication services" << "\n" << "Course work on MSKvSPD" << "\n" << "Student group: BSU1401" << "\n" << "Developers:" << " " << "Denis Petuhov" << "," << " " << "etc." << "\n" << "\n" << ed.applicationName() << "." << " " << "Version" << " " << ed.applicationVersion() << "\n" << "\n"
-                      << "Usage:" << " " << argv[0] << " " << "[-e string] [-d code]" << "\n"
+    WelcomeMessageQSL << "Moscow Technical University of Communication and Informatics" << "\n" << "Department of multimedia networks and communication services" << "\n" << "Course work on MSKvSPD" << "\n" << "Student group: BSU1401" << "\n"
+                      << "Developers:" << " " << "Denis Petuhov" << "," << "\n"
+                      << "            " << "Korotygin Aleksandr" << "," << "\n"
+                      << "            " << "Kaledina Anastasija" << "," << "\n"
+                      << "            " << "Gubankova Ekaterina" << "." << "\n" << "\n"
+                      << ed.applicationName() << "." << " " << "Version" << " " << ed.applicationVersion() << "\n" << "\n"
+                      << "Usage:" << " " << argv[0] << " " << "[-e image file to encode] [-d image file to encode]" << "\n"
+                      << "Supported file image formats: *.tif, *.tiff" << "\n" << "\n"
                       << helptext << "\n" "\n";
     QString WelcomeMessage = (QString) WelcomeMessageQSL.join("");
     out << WelcomeMessage;
