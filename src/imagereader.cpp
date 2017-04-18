3b1795c6fea7bf6aa9eed68fc3b949c0619bb6f9
Denis <zmreact@gmail.com>
2017-04-18 23:08:55 +0300

    Убрал обработку пикселей

--- src/imagereader.cpp
+++ src/imagereader.cpp
@@ -12,18 +12,5 @@ bool readimageblock(QTextStream& out, QString path)
         out << "Height: " << img.height() << endl;
         out << "Depth: " << img.bitPlaneCount() << endl;
         out << "Bytes per line: " << img.bytesPerLine() << endl;
-
-        for (int i = 0; i < img.height(); i++)
-        {
-            QRgb *rowData = (QRgb *) img.scanLine(i);
-            for (int j = 0; j < img.width(); j++)
-            {
-                QRgb pixelData = rowData[j];
-                int red = qRed(pixelData),
-                    green = qGreen(pixelData),
-                    blue = qBlue(pixelData);
-                out << i*img.width()+j+1 << " " << "pixel data: " << "R" << red << "G" << green << "B" << blue << endl;
-            }
-        }
      } else return false;
 }
